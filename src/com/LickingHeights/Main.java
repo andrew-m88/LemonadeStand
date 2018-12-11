fdb5fedc1eb508347fb1036083b6bc05dd3313dd
Andrew Motley <andrew.motley.2021@student.lhschools.org>
2018-12-11 08:10:44 -0500

    worked on project in intellij

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,33 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int sugar;
+        int lemon;
+        int cup;
+        int days;
+        String items;
+        double cupcost;
+        double lemoncost;
+        double sugarcost;
+        double totalcost;
+        String name;
+        Scanner keyboard;
+
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Hello, welcome to the lemonade stand");
+        System.out.println("What is your name");
+        name = keyboard.nextLine();
+        System.out.println("well, nice to meet you " + name);
+        System.out.println("How many items do you need today?(Three or Four)");
+        items = keyboard.nextLine();
+        if (items.equals("three")) {
+
+        }
     }
 }
